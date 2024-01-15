f25faad491cfd4cb0019eccb1605e09449aece0f
Aman Sharma <halfengiiineeer2022@gmail.com>
2024-01-15 17:27:23 +0530

    first

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBIx0diHry1VFr2DuNJn4mBVO3idpts4Wk",
+  authDomain: "netflix-3b15f.firebaseapp.com",
+  projectId: "netflix-3b15f",
+  storageBucket: "netflix-3b15f.appspot.com",
+  messagingSenderId: "125094065852",
+  appId: "1:125094065852:web:f4c292890b510bf93702b9",
+  measurementId: "G-JE24WY19X6",
+};
